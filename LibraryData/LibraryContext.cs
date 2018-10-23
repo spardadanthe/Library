87a0ff70e78c977b227f9bc1cddfd4eb6165a40b
spardadanthe <sparda.danthe@gmail.com>
2018-10-23 16:33:23 +0300

    Database Created

---
+++ LibraryData/LibraryContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LibraryData.Models;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Patron> Patron { get; set; }
+    }
+}
